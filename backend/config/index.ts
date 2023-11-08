046bc98267937e6e0ca5b123fac9515603d629e6
Diego <diego2000avelar@gmail.com>
2023-11-08 09:00:36 -0600

    feat: prepair frontend

--- backend/config/index.ts
+++
@@ -1,5 +0,0 @@
-// app.ts 
-import dotenv from 'dotenv'; 
-dotenv.config();  // Load environment variables from .env file 
-export const INFURA_APP_ID = process.env.INFURA_APP_ID; 
-export const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS; 
